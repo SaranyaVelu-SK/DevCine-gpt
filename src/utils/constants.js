d928c95858354121dca31d0cc2dc3d957d2dc543
SaranyaSK <saranyakulanthaivelu@gmail.com>
2025-07-10 15:11:58 +0530

    done safety

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,5 @@
 export const USER_PHOTO_URL = "https://img.icons8.com/parakeet/48/user-male-circle.png";
 export const API_GET_OPTIONS_FOR_TMDB = {
   method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZjEzMWNmOWJlYjdkNjQ0YzRkYjhlMmRmZDg1MTFkMCIsIm5iZiI6MTc1MjA1MDYxNC41NzEsInN1YiI6IjY4NmUyYmI2NjgwMTY5NGY0MTEwNDA2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RzHgEt0dUmEXd937Sc8I1XB2u197galFdMQ4DqS1Bo4'
-  }
+  headers: import.meta.env.VITE_AUTHORIZATION
 };
